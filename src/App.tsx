883dee64b52b83e5d5c824482ba41017d30785de
Karl von Randow <karl@xk72.com>
2017-12-14 14:18:07 +1300

    App.tsx: use Redux and our example component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from './modules/index';
+
 import './App.css';
+import Example from './modules/template/containers/example';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Provider store={store}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <Example />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      </Provider>
     );
   }
 }
