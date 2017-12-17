8e5d13699cae08591aa86b631ca735885545a8ba
Karl von Randow <karl@xk72.com>
2017-12-18 07:33:01 +1300

    Enable auth saga

--- src/modules/sagas.ts
+++ src/modules/sagas.ts
@@ -4,9 +4,13 @@
 
 import { all } from 'redux-saga/effects';
 
+/* Import module sagas */
+import authSaga from './auth/sagas';
+
 /** The root saga that starts all of the other sagas. */
 export default function* rootSaga() {
     yield all([
-        
+        /* The authentication saga */
+        authSaga(),
     ]);
 }
