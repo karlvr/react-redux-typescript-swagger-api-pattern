ee3f5da9a40c3248b76ccb22eced4c1e8c57b19e
Karl von Randow <karl@xk72.com>
2019-07-10 13:03:16 +1200

    Generate api using openapi-generator

---
+++ gen/api/dist/models/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./Category"));
+__export(require("./ModelApiResponse"));
+__export(require("./Order"));
+__export(require("./Pet"));
+__export(require("./Tag"));
+__export(require("./User"));
