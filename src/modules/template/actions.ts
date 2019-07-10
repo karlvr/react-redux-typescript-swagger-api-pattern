9856889be226542f0960e3e59b5ad8b3a4a48e05
Karl von Randow <karl@xk72.com>
2019-07-10 13:45:50 +1200

    Modernise actions template

--- src/modules/template/actions.ts
+++ src/modules/template/actions.ts
@@ -1,24 +1,24 @@
-import actionCreatorFactory, { Action } from 'typescript-fsa';
+import actionCreatorFactory from 'typescript-fsa'
 
 /**
  * The action creator for this module. Note that it contains the module name.
  */
-const actionCreator = actionCreatorFactory('Template');
+const actionCreator = actionCreatorFactory('Template')
 
 /* An example action with an interface as its payload. */
 export interface ExampleActionPayload {
-    value: string;
+	value: string
 }
-export const exampleAction = actionCreator<ExampleActionPayload>('EXAMPLE');
+export const exampleAction = actionCreator<ExampleActionPayload>('EXAMPLE')
 
 /* An example action with a primitive payload. */
-export const examplePrimitiveAction = actionCreator<string>('EXAMPLE_PRIMITIVE');
+export const examplePrimitiveAction = actionCreator<string>('EXAMPLE_PRIMITIVE')
 
 /* An example action with more types that can be used to use typing with this
 action elsewhere in the code.
  */
 export interface ExampleWithMoreTypesActionPayload {
-    value: string;
+	value: string
 }
-export type ExampleWithMoreTypesAction = Action<ExampleWithMoreTypesActionPayload>;
-export const exampleWithMoreTypesAction = actionCreator<ExampleWithMoreTypesActionPayload>('EXAMPLE_MORE_TYPES');
+export const exampleWithMoreTypesAction = actionCreator<ExampleWithMoreTypesActionPayload>('EXAMPLE_MORE_TYPES')
+export type ExampleWithMoreTypesAction = ReturnType<typeof exampleWithMoreTypesAction>
